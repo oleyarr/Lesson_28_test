2f58c0f6b7c84828c2ae17895a2ecf74e2981de0
Володя <>
2021-07-28 19:57:47 +0300

    Add lesson_28 project

---
+++ Lesson_28/Lesson_28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_28
+//
+//  Created by Володя on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
